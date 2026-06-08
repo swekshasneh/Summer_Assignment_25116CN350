feff0ac57e2848aaa946848249217159cf55e8ba
Sweksha Sneh <25116cn350@glbitm.ac.in>
2026-06-08 14:01:44 +0530

    Day-8 assignment is uploaded

---
+++ Day-8/Write a program to Print character triangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int rows = 5;
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << char('A' + j);
+        }
+        cout << '\n';
+    }
+    return 0;
+}
