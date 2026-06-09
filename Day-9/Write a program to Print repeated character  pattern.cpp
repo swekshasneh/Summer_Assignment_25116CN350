8d4f823803761557b8d5366b9a8c05d59002411c
Sweksha Sneh <25116cn350@glbitm.ac.in>
2026-06-09 14:25:15 +0530

    Day-9 assignment is uploaded

---
+++ Day-9/Write a program to Print repeated character  pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        char ch = 'A' + (i - 1);
+        for (int j = 0; j < i; j++) {
+            cout << ch;
+        }
+        cout << endl;
+    }
+    return 0;
+}
