ab0efe5303069876e0e901140afeb2bed75d701e
Sweksha Sneh <25116cn350@glbitm.ac.in>
2026-06-11 10:44:21 +0530

    Day-11 assignment is uploaded

---
+++ Day-11/Write a program to Write function to find maximum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int maximum(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Maximum number = " << maximum(num1, num2);
+
+    return 0;
+}
---
+++ Day-11/Write a program to Write function to find sum of two numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Sum = " << sum(num1, num2);
+
+    return 0;
+}
