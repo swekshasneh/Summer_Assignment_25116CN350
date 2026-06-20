0f35230d1117aff2ea02a012f98fb401a0164470
Sweksha Sneh <25116cn350@glbitm.ac.in>
2026-06-20 22:57:05 +0530

    Day-20 assignment is uploaded

---
+++ Day-20/Write a program to Check symmetric matrix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout << "Enter the order of square matrix: ";
+    cin >> n;
+
+    int matrix[10][10];
+    bool symmetric = true;
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // Check symmetry
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                symmetric = false;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        cout << "Matrix is Symmetric";
+    else
+        cout << "Matrix is Not Symmetric";
+
+    return 0;
+}
